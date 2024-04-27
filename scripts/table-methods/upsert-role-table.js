2fc4537fde7c12b3b29346860030053a53ecbc01
Michael J. Walker <neutrix@brandgeneering.com>
2024-04-27 16:30:30 +0100

    works up to find a record

--- scripts/table-methods/upsert-role-table.js
+++ scripts/table-methods/upsert-role-table.js
@@ -8,8 +8,108 @@ const { Role_Key,Role_Value } = inputConfig;
 const Role_ID = Role_Value[0];
 // console.log({ Role_Key,Role_ID, Role_Value}) //** Inspect */
 
-// create list of field ids excluding the Roles
-const getFilteredFields = (config) => {
+// Get the fields
+//** Select the Field names */
+const getFields = (config) => {
+  const fields = Object.keys(config).filter(field => {
+    return (
+      !field.includes("input")
+      && !field.includes("Role")
+    )
+  });
+  return fields; // add Role Key to output
+}
+
+const testFields = getFields(inputConfig);
+console.log({ testFields }) //** Inspect */
+
+//** Get the records */
+const asyncGetRecords = async (fieldArray) => {
+  const records = await table.selectRecordsAsync(
+    { fields: fieldArray }
+  );
+  return records;
+}
+//
+const fieldsWithRole = [...testFields,Role_Key];
+const testRecords = await asyncGetRecords(fieldsWithRole)
+console.log({ testRecords }) //** Inspect */
+
+//** Find record by Role ID */
+const asyncFindRecordByRoleId = (records,Role_ID) => {
+  const foundRecords = records.records.find(record => {
+    const record_Role = record.getCellValue(Role_Key)[0];
+    return record_Role.id === Role_ID;
+  })
+  return foundRecords;
+}
+const testFindRecs = await asyncFindRecordByRoleId(testRecords,Role_ID)
+console.log({ testFindRecs }) //** Inspect */
+
+// update
+
+// ************************************************
+
+// find the records for a linked record
+const records = await table.selectRecordsAsync(
+  { fields: [Role_Key] }
+);
+
+// select record based on the Role_Value  
+const foundRecords = records.records.find(record => {
+  const record_Role = record.getCellValue(Role_Key)[0];
+  return record_Role.id === Role_ID;
+})
+// console.log({ records, foundRecords }) //** Inspect */
+
+// update base field filtered array
+function asyncCreateInputsFromKeys(fieldArray) {
+  let inputs = {};
+  fieldArray.forEach(key => {
+    const value = inputConfig[key][0];
+    inputs[key] = value
+
+    if (key === "searchable_id") {
+      inputs[key] = value ? value.trim().toLowerCase() : null;
+    }
+  })
+
+  return inputs;
+}
+// ======================================================= 
+async function asyncProcessRecords() {
+  let Record_ID = null;
+  let Action_Status = null;
+
+  try {
+    if (foundRecords) {
+
+
+      // update the link records
+      const recordID = foundRecords.id;
+      const updatedRecord = await table.updateRecordAsync(recordID,{
+        [Role_Key]: [{ id: Role_ID }]
+      });
+
+    } else {
+      // create a new record
+      const newRecord = await table.createRecordAsync({
+        [Role_Key]: [{ id: Role_ID }]
+      });
+      // console.log({ newRecord }) //** Inspect */
+
+    }
+
+    return { Record_ID,Action_Status }
+  } catch (error) {
+
+
+  }
+}
+
+
+create list of field ids excluding the Roles
+const filterBasicFields = (config) => {
   const fields = Object.keys(config).filter(field => {
     return (
       !field.includes("Role") // excludes Role fields
@@ -20,10 +120,10 @@ const getFilteredFields = (config) => {
   });
   return fields;
 }
-const filteredFields = getFilteredFields(inputConfig);
+const filteredFields = filterBasicFields(inputConfig);
 console.log({ filteredFields }) //** Inspect */
 
-const getFilteredRecordLinks = (config) => {
+const filterLinkedFields = (config) => {
   const recordLinks = Object.keys(config).filter(field => {
     return (
       field.includes("ID_") // includes linked fields
@@ -31,69 +131,5 @@ const getFilteredRecordLinks = (config) => {
   });
   return recordLinks;
 }
-const filteredRecordLinks = getFilteredRecordLinks(inputConfig);
-console.log({ filteredRecordLinks }) //** Inspect */
-
-// const getSelectedRecords = async (inputConfig) => {
-//   const records = await table.selectRecordsAsync(
-//     { fields: filteredFields }
-//   );
-//   return records;
-// }
-
-// // find the records for a linked record
-// const records = await table.selectRecordsAsync(
-//   { fields: [Role_Key] }
-// );
-
-// // select record based on the Role_Value  
-// const foundRecords = records.records.find(record => {
-//   const record_Role = record.getCellValue(Role_Key)[0];
-//   return record_Role.id === Role_ID;
-// })
-// // console.log({ records, foundRecords }) //** Inspect */
-
-// // update base field filtered array
-// function asyncCreateInputsFromKeys(fieldArray) {
-//   let inputs = {};
-//   fieldArray.forEach(key => {
-//     const value = inputConfig[key][0];
-//     inputs[key] = value
-
-//     if (key === "searchable_id") {
-//       inputs[key] = value ? value.trim().toLowerCase() : null;
-//     }
-//   })
-
-//   return inputs;
-// }
-// // ======================================================= 
-// async function asyncProcessRecords() {
-//   let Record_ID = null;
-//   let Action_Status = null;
-
-//   try {
-//     if (foundRecords) {
-
-
-//       // update the link records
-//       const recordID = foundRecords.id;
-//       const updatedRecord = await table.updateRecordAsync(recordID,{
-//         [Role_Key]: [{ id: Role_ID }]
-//       });
-
-//     } else {
-//       // create a new record
-//       const newRecord = await table.createRecordAsync({
-//         [Role_Key]: [{ id: Role_ID }]
-//       });
-//       // console.log({ newRecord }) //** Inspect */
-
-//     }
-
-//     return { Record_ID,Action_Status }
-//   } catch (error) {
-
-
-//   }
-// }
+const filteredLinks = filterLinkedFields(inputConfig);
+console.log({
