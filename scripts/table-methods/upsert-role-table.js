0bca18c909918d54a8ffda525267eacfc9c61e20
Michael J. Walker <neutrix@brandgeneering.com>
2024-04-27 14:40:31 +0100

    make filtered fields a fn

--- scripts/table-methods/upsert-role-table.js
+++ scripts/table-methods/upsert-role-table.js
@@ -3,53 +3,88 @@
 const inputConfig = input.config();
 const table = base.getTable(inputConfig.input_Table_Name);
 
-// get Role ID and Label
+// @ts-ignore
 const { Role_Key,Role_Value } = inputConfig;
 const Role_ID = Role_Value[0];
 // console.log({ Role_Key,Role_ID, Role_Value}) //** Inspect */
 
 // create list of field ids excluding the Roles
-const filteredFields = Object.keys(inputConfig).filter(field => {
-  return (
-    !field.includes("Role") // excludes Role fields
-    && !field.includes("ID_") // excludes linked fields
-    && !field.includes("is_") // excludes logic fields
-  );
-});
+const getFilteredFields = () => {
+  const fields = Object.keys(inputConfig).filter(field => {
+    return (
+      !field.includes("Role") // excludes Role fields
+      && !field.includes("input") // excludes input fields
+      && !field.includes("ID_") // excludes linked fields
+      && !field.includes("is_") // excludes logic fields
+    );
+  });
+  return fields;
+}
+
+const filteredFields = getFilteredFields();
 console.log({ filteredFields }) //** Inspect */
 
-// find the records for a linked record
-const filteredRecordLinks = Object.keys(inputConfig).filter(field => {
-  return (
-    field.includes("ID_") // includes linked fields
-  );
-});
-console.log({ filteredRecordLinks }) //** Inspect */
+// // find the records for a linked record
+// const filteredRecordLinks = Object.keys(inputConfig).filter(field => {
+//   return (
+//     field.includes("ID_") // includes linked fields
+//   );
+// });
+// console.log({ filteredRecordLinks }) //** Inspect */
 
-// find the records for a linked record
-const records = await table.selectRecordsAsync(
-  { fields: [Role_Key] }
-);
+// // find the records for a linked record
+// const records = await table.selectRecordsAsync(
+//   { fields: [Role_Key] }
+// );
 
-// select record based on the Role_Value  
-const foundRecords = records.records.find(record => {
-  const record_Role = record.getCellValue(Role_Key)[0];
-  return record_Role.id === Role_ID;
-})
-// console.log({ records, foundRecords }) //** Inspect */
+// // select record based on the Role_Value  
+// const foundRecords = records.records.find(record => {
+//   const record_Role = record.getCellValue(Role_Key)[0];
+//   return record_Role.id === Role_ID;
+// })
+// // console.log({ records, foundRecords }) //** Inspect */
 
-if (foundRecords) {
-  // update the record
-  const recordID = foundRecords.id;
-  const updatedRecord = await table.updateRecordAsync(recordID,{
-    [Role_Key]: [{ id: Role_ID }]
-  });
+// // update base field filtered array
+// function asyncCreateInputsFromKeys(fieldArray) {
+//   let inputs = {};
+//   fieldArray.forEach(key => {
+//     const value = inputConfig[key][0];
+//     inputs[key] = value
 
-} else {
-  // create a new record
-  const newRecord = await table.createRecordAsync({
-    [Role_Key]: [{ id: Role_ID }]
-  });
-  // console.log({ newRecord }) //** Inspect */
+//     if (key === "searchable_id") {
+//       inputs[key] = value ? value.trim().toLowerCase() : null;
+//     }
+//   })
 
-}
+//   return inputs;
+// }
+// // ======================================================= 
+// async function asyncProcessRecords() {
+//   let Record_ID = null;
+//   let Action_Status = null;
+
+//   try {
+//     if (foundRecords) {
+
+
+//       // update the link records
+//       const recordID = foundRecords.id;
+//       const updatedRecord = await table.updateRecordAsync(recordID,{
+//         [Role_Key]: [{ id: Role_ID }]
+//       });
+
+//     } else {
+//       // create a new record
+//       const newRecord = await table.createRecordAsync({
+//         [Role_Key]: [{ id: Role_ID }]
+//       });
+//       // console.log({ newRecord }) //** Inspect */
+
+//     }
+
+//     return { Record_ID,Action_Status }
+//   } catch (error) {
+
+
+//   }
+// }
