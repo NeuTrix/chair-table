b5d8e0eb2e3ee411f4e4e8ed202787baf180045d
Michael J. Walker <neutrix@brandgeneering.com>
2024-04-27 14:24:31 +0100

    set field filters

--- scripts/table-methods/upsert-role-table.js
+++ scripts/table-methods/upsert-role-table.js
@@ -8,6 +8,24 @@ const { Role_Key,Role_Value } = inputConfig;
 const Role_ID = Role_Value[0];
 // console.log({ Role_Key,Role_ID, Role_Value}) //** Inspect */
 
+// create list of field ids excluding the Roles
+const filteredFields = Object.keys(inputConfig).filter(field => {
+  return (
+    !field.includes("Role") // excludes Role fields
+    && !field.includes("ID_") // excludes linked fields
+    && !field.includes("is_") // excludes logic fields
+  );
+});
+console.log({ filteredFields }) //** Inspect */
+
+// find the records for a linked record
+const filteredRecordLinks = Object.keys(inputConfig).filter(field => {
+  return (
+    field.includes("ID_") // includes linked fields
+  );
+});
+console.log({ filteredRecordLinks }) //** Inspect */
+
 // find the records for a linked record
 const records = await table.selectRecordsAsync(
   { fields: [Role_Key] }
@@ -18,5 +36,20 @@ const foundRecords = records.records.find(record => {
   const record_Role = record.getCellValue(Role_Key)[0];
   return record_Role.id === Role_ID;
 })
+// console.log({ records, foundRecords }) //** Inspect */
 
-console.log({ records,foundRecords }) //** Inspect */
+if (foundRecords) {
+  // update the record
+  const recordID = foundRecords.id;
+  const updatedRecord = await table.updateRecordAsync(recordID,{
+    [Role_Key]: [{ id: Role_ID }]
+  });
+
+} else {
+  // create a new record
+  const newRecord = await table.createRecordAsync({
+    [Role_Key]: [{ id: Role_ID }]
+  });
+  // console.log({ newRecord }) //** Inspect */
+
+}
